c1d24051c975635014f1c25ce76a4e80dbd856eb
Anders Spælling <anders.spaelling@lumagate.com>
2016-10-04 16:58:03 +0200

    created service
    https://msdn.microsoft.com/en-us/library/zt39148a(v=vs.110).aspx

---
+++ code/service/scwebhook/scwebhook/scwebhook/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scwebhook
+{
+    public static class Constants
+    {
+        public const string EVENT_LOG_SOURCE = "ScWebhooks";
+        public const string EVENT_LOG_NAME = "ScWebhooks";
+    }
+}
---
+++ code/service/scwebhook/scwebhook/scwebhook/Scwebhook.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+
+namespace scwebhook
+{
+    public partial class Scwebhook : ServiceBase
+    {
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);
+
+        public enum ServiceState
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public long dwServiceType;
+            public ServiceState dwCurrentState;
+            public long dwControlsAccepted;
+            public long dwWin32ExitCode;
+            public long dwServiceSpecificExitCode;
+            public long dwCheckPoint;
+            public long dwWaitHint;
+        };
+
+        public Scwebhook()
+        {
+            InitializeComponent();
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists(Constants.EVENT_LOG_SOURCE))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    Constants.EVENT_LOG_SOURCE, Constants.EVENT_LOG_NAME);
+            }
+            eventLog1.Source = Constants.EVENT_LOG_SOURCE;
+            eventLog1.Log = Constants.EVENT_LOG_NAME;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart");
+
+            // Update the service state to Start Pending.
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            // Set up a timer to trigger every minute.
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 60000; // 60 seconds
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+
+            // Update the service state to Running.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In onStop.");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("In OnContinue.");
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            int eventId = 42;
+            // TODO: Insert monitoring activities here.
+            eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId);
+        }
+    }
+}
